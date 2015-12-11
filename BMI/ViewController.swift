9d9a2f95408aefbd4155c603131265f3c81c89cc
slpedro <slpedro@hotmail.com>
2015-12-11 13:39:48 +0100

    Initial Commit

---
+++ BMI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BMI
+//
+//  Created by Pedro Sánchez Luling on 11/12/15.
+//  Copyright © 2015 Pedro Sánchez Luling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
